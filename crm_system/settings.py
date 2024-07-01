f1f2923aeb3b9fd917451417c3be5a5d8374dc8a
dimastepanov <dimastepanov1980@gmail.com>
2024-07-01 13:15:54 +0700

    Updated settings and fixed bugs

--- crm_system/settings.py
+++ crm_system/settings.py
@@ -113,6 +113,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

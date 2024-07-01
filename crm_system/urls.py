1a3f2d73c89089f31e029de833cae11d8491eac7
dimastepanov <dimastepanov1980@gmail.com>
2024-07-01 11:59:21 +0700

    Updated requirements and configuration for Heroku

--- crm_system/urls.py
+++ crm_system/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from core import views
 
 urlpatterns = [
-    path('', views.HomePageView, name='home'),
     path('admin/', admin.site.urls),
     path('', include('core.urls')),  # Включение URL-адресов приложения core
 ]
